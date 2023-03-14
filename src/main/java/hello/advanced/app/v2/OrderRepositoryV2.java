5d6df2422032a4979dd2a74be7b7984d09875c8e
kyunghwan1207 <kyunghwanko1207@gmail.com>
2023-03-14 16:05:16 +0900

    feat: v2개발-log출력에 필요한 traceId정보를 파라미터로 전달

---
+++ src/main/java/hello/advanced/app/v2/OrderRepositoryV2.java
@@ -0,0 +1,37 @@
+package hello.advanced.app.v2;
+
+import hello.advanced.trace.TraceId;
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
+import hello.advanced.trace.hellotrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV2 {
+    private final HelloTraceV2 trace;
+    public void save(TraceId traceId, String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.beginSync(traceId, "OrderRepositoryV2-save");
+            // 저장 로직
+            if (itemId.equals("ex")) {
+                throw new IllegalStateException("예외 발생!");
+            }
+            sleep(1000); // 저장할 때 1초 걸린다는 가정
+
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; // 예외를 꼭 다시 던져줘야한다! 여기서 에러 먹어버리면 안됨!
+        }
+
+    }
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
