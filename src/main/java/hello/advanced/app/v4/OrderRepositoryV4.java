0c1bfbe8a7ea19e7a7205626b7bd97f4156bd8ef
kyunghwan1207 <kyunghwanko1207@gmail.com>
2023-03-15 16:53:16 +0900

    refactor: 탬탬플릿 메서드 패턴 적용

---
+++ src/main/java/hello/advanced/app/v4/OrderRepositoryV4.java
@@ -0,0 +1,35 @@
+package hello.advanced.app.v4;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logTrace.LogTrace;
+import hello.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV4 {
+    private final LogTrace trace;
+
+    public void save(String itemId) {
+        AbstractTemplate<Void> template = new AbstractTemplate<>(trace) {
+            @Override
+            protected Void call() {
+                // 저장 로직
+                if (itemId.equals("ex")) {
+                    throw new IllegalStateException("예외 발생!");
+                }
+                sleep(1000); // 저장할 때 1초 걸린다는 가정
+                return null;
+            }
+        };
+        template.execute("OrderControllerV4-request");
+    }
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/hello/advanced/app/v4/OrderServiceV4.java
@@ -0,0 +1,25 @@
+package hello.advanced.app.v4;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logTrace.LogTrace;
+import hello.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV4 {
+    private final LogTrace trace;
+    private final OrderRepositoryV4 orderRepository;
+
+    public void orderItem(String itemId) {
+        AbstractTemplate<Void> template = new AbstractTemplate<>(trace) {
+            @Override
+            protected Void call() {
+                orderRepository.save(itemId);
+                return null;
+            }
+        };
+        template.execute("OrderControllerV4-orderItem");
+    }
+}
